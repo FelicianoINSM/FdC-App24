6788865708a1326af1da53d6364d359a46f85342
Feliciano Romero <felicianoromero.sec@institutolamerced.edu.ar>
2024-03-20 21:14:31 -0300

    HotFix

--- Server/main.py
+++ Server/main.py
@@ -10,7 +10,7 @@ class Listener:
     
     def run(self):
         self.rules()
-        self.app.run(host="0.0.0.0", port=8080, debug=True)
+        self.app.run(host="0.0.0.0", port=5000, debug=True)
 
 if __name__ == '__main__':
     Listener().run()
