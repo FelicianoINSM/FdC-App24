b926acef607fef467db05d3ae3977b5c9c7f3b18
Feliciano Romero <felicianoromero.sec@institutolamerced.edu.ar>
2024-03-29 11:29:50 -0300

    New routes added to the API

--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes import Home
+from routes import Home, Daily, Status
 
 class Listener:
     def __init__(self):
@@ -7,6 +7,8 @@ class Listener:
 
     def rules(self):
         self.app.add_url_rule('/', view_func=Home.as_view('home'))
+        self.app.add_url_rule('/v1/daily', view_func=Daily.as_view('daily'))
+        self.app.add_url_rule('/v1/status', view_func=Status.as_view('status'))
     
     def run(self):
         self.rules()
--- Server/routes.py
+++ Server/routes.py
@@ -1,12 +1,27 @@
 from flask.views import MethodView
 from flask import request, make_response, jsonify, render_template
+from datetime import datetime
 
 class Home(MethodView):
     def get(self):
         return render_template('index.html')
-    # def post(self):
-    #     data = request.get_json()
-    #     if data["msg"]:
-    #         return data["msg"]
-    #     else:
-    #         return None
+
+class Daily(MethodView):
+    def get(self):
+        data = {
+            "temp": 32,
+            "f_hum": 20,
+            "e_hum": 45,
+            "last": datetime.now()
+        }
+        return jsonify(data)
+    
+class Status(MethodView):
+    def get(self):
+        return jsonify({
+            "aspersores":True
+        })
+    def post(self):
+        data = request.json
+        print(data)
+        return jsonify("Receieved")
