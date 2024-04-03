6fab955fe711afa4a637b6326709e4faad4cbc95
Gonzalo Boffa <gonzaloboffa.sec@institutolamerced.edu.ar>
2024-04-03 17:36:33 -0300

    La direccion del servidor se guarda

--- App/main.py
+++ App/main.py
@@ -1,5 +1,8 @@
-
 from connection import Connect
+from kivy.uix.screenmanager import Screen
+import os
+import configparser
+
 
 import kivy
 kivy.require('2.3.0')
@@ -18,7 +21,29 @@ class Menu(Screen):
     pass
         
 class Configuracion(Screen):
-    pass
+    def save_server_address(self):
+        server_address = self.ids.server_address_input.text.strip()
+        
+        if not server_address:
+            print("Error: Por favor, ingrese una dirección de servidor válida.")
+            return
+
+        config = configparser.ConfigParser()
+
+        config['Servidor'] = {'direccion': server_address}
+
+        try:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            config_file_path = os.path.join(dir_path, 'configuracion.cfg')
+
+            with open(config_file_path, 'w') as configfile:
+                config.write(configfile)
+
+            print("Dirección del servidor guardada:", server_address)
+        except Exception as e:
+            print("Error al escribir en el archivo de configuración:", e)
+
+        print("Dirección del servidor guardada:", server_address)
 
 class TabManager(ScreenManager):
     pass
