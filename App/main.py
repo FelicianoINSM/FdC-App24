54e3947c3a5bc1e29970a94ce8891e6755bd8a2a
Gonzaloboffa25 <163351002+Gonzaloboffa25@users.noreply.github.com>
2024-04-08 17:22:13 -0300

    Servidor y puerto

--- App/main.py
+++ App/main.py
@@ -1,3 +1,4 @@
+import re
 from connection import Connect
 from kivy.uix.screenmanager import Screen
 import os
@@ -22,15 +23,29 @@ class Menu(Screen):
         
 class Configuracion(Screen):
     def save_server_address(self):
+        # Expresión regular para capturar la dirección del servidor y el número de puerto
+        regex = r'([a-zA-Z0-9.-]+):(\d+)'
+
         server_address = self.ids.server_address_input.text.strip()
-        
+
         if not server_address:
             print("Error: Por favor, ingrese una dirección de servidor válida.")
             return
 
+        # Verificar si la dirección del servidor coincide con la expresión regular
+        match = re.match(regex, server_address)
+        if not match:
+            print("Error: La dirección del servidor debe tener el formato 'direccion:puerto'.")
+            return
+
+        # Extraer la dirección del servidor y el número de puerto del grupo de coincidencia
+        server_address = match.group(1)
+        port_number = match.group(2)
+
         config = configparser.ConfigParser()
 
-        config['Servidor'] = {'direccion': server_address}
+        config['Servidor'] = {'direccion': server_address,
+                              'puerto': port_number}
 
         try:
             dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -39,11 +54,11 @@ class Configuracion(Screen):
             with open(config_file_path, 'w') as configfile:
                 config.write(configfile)
 
-            print("Dirección del servidor guardada:", server_address)
+            print("Dirección del servidor y número de puerto guardados:", server_address, ":", port_number)
         except Exception as e:
             print("Error al escribir en el archivo de configuración:", e)
 
-        print("Dirección del servidor guardada:", server_address)
+        print("Dirección del servidor y número de puerto guardados:", server_address, ":", port_number)
 
 class TabManager(ScreenManager):
     pass
