d50ef9fb3c6cca7f37a91ba979348de0b4309338
MILI <milagrosmiani.sec@institutolamerced.edu.ar>
2024-06-03 09:15:54 -0300

    terminado¿

--- App/main.py
+++ App/main.py
@@ -1,5 +1,8 @@
 
+import requests
 from connection import Connect
+from datetime import datetime
+
 
 import kivy
 kivy.require('2.3.0')
@@ -11,8 +14,23 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen
+
 class Home(Screen):
-    pass
+    switch_button = ObjectProperty(None)
+    temp = ObjectProperty(None)
+    env_humidity = ObjectProperty(None)
+    humidity = ObjectProperty(None)
+    last_time = ObjectProperty(None)
+
+    def switch_button_pressed(self):
+        switch_button = self.ids.switch_button
+        if switch_button.text == "Encender":
+            switch_button.text = "Apagar"
+        else:
+            switch_button.text = "Encender"
+    
 
 class Menu(Screen):
     pass
@@ -23,9 +41,24 @@ class TabManager(ScreenManager):
 kv = Builder.load_file('main.kv')
 
 class Riego(App):
-
     def build(self):
-        return kv
+            return kv
+    def on_start(self):
+            data = requests.get("http://127.0.0.1:5000/v1/daily").json()
+            temperatura = str(data["temp"])
+            humedad_ambiente = str(data["e_hum"])
+            humedad_suelo = str(data["f_hum"])
+            ultimo_riego = data["last"]
+          
+            home_screen = self.root.get_screen('home')
+            home_screen.temp.text = temperatura +"°"
+            home_screen.env_humidity.text = humedad_ambiente + "%"
+            home_screen.humidity.text = humedad_suelo + "%"
+            home_screen.last_time.text = ultimo_riego + "hs"
+
+    
+
+    
 
 if __name__ == '__main__':
     riegoApp = Riego()
